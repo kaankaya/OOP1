542f9d2c37af0504dd6466283c5a394e8eb66fcd
Kaan Kaya <kaankaya.0707@gmail.com>
2024-07-30 04:04:26 +0300

    Add project files.

---
+++ OOP1/Person.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class Person
+    {
+        //Field tanımları
+        private string name;
+        private string surname;
+        private int dateOfBirth;
+        private string role;
+        //Field lara Property Tanımlamaları
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }//End Property Name
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }//End Property Surname
+
+        public int DateOfBirth
+        {
+            get
+            {
+                return dateOfBirth;
+            }
+            set
+            {
+                if(value > 0)
+                {
+                    dateOfBirth = value;
+                }
+                else
+                {
+                    Console.WriteLine("Yanlış Doğum Yılı Girdiniz");
+                }
+            }
+        }//End Propert DateOfBirth
+
+        public string Role
+        {
+            get
+            {
+                return role;
+            }
+            set
+            {
+                role = value;
+            }
+        }//End property Role
+
+        //Listeleme Methodumuz
+        public void ListPeople()
+        {
+            Console.WriteLine($"Alanı : {role}");
+            Console.WriteLine($"Adı : {name}");
+            Console.WriteLine($"Soyadı : {surname}");
+            Console.WriteLine($"Doğum Yılı : {dateOfBirth}");
+            Console.WriteLine("*****************************");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,51 @@
+﻿using OOP1;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //nesnemizi oluşturuyoruz
+        //we are creating our object
+        Person person = new Person();
+
+        //Tanımlamaları yapıyoruz
+        //we are defining the properties
+        person.Name = "Sertan";
+        person.Surname = "Bozkuş";
+        person.DateOfBirth = 1993;
+        person.Role = "Öğrenci";
+        person.ListPeople();
+        
+
+
+        person.Name = "Aleyna";
+        person.Surname = "Avcı";
+        person.DateOfBirth = 1995;
+        person.Role = "Öğrenci";
+        person.ListPeople();
+        
+
+        person.Name = "Murat";
+        person.Surname = "Gögebakan";
+        person.DateOfBirth = 1970;
+        person.Role = "Öğrenci";
+        person.ListPeople();
+
+        person.Name = "Hayri";
+        person.Surname = "Tutucu";
+        person.DateOfBirth = 1985;
+        person.Role = "Öğretmen";
+        person.ListPeople();
+
+        person.Name = "Mahmut";
+        person.Surname = "Tuncer";
+        person.DateOfBirth = 1970;
+        person.Role = "Öğretmen";
+        person.ListPeople();
+
+       
+
+
+
+    }
+}
